eacd48b3a24be3fba865163b6cf5aa48a3a73074
Hazem Khairy <hazem.hassan.kh@gmail.com>
2023-05-31 00:02:19 +0000

    Add SQL queries for table creation

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -6,22 +6,88 @@ from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
                                 LoadDimensionOperator, DataQualityOperator)
 from helpers import SqlQueries
 
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
+AWS_KEY = os.environ.get('AWS_KEY')
+AWS_SECRET = os.environ.get('AWS_SECRET')
 
+# Setup the default args according to the project instructions
 default_args = {
     'owner': 'udacity',
     'start_date': datetime(2019, 1, 12),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup_by_default': False,
+    'email_on_retry': False
 }
 
+# TODO: What's the desired schedule?
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
+          schedule_interval='@daily'
         )
 
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+start_operator = DummyOperator(
+    task_id='Begin_execution',
+    dag=dag
+)
 
+################################################# Table Creation tasks ################################
+create_staging_events_table_task = PostgresOperator(
+    task_id = "Create Staging Events Table, if not exists",
+    dag = dag,
+    postgres_conn_id = "redshift",
+    sql = SqlQueries.staging_events_table_create
+)
+
+create_staging_songs_table_task = PostgresOperator(
+    task_id = "Create Staging Songs Table, if not exists",
+    dag = dag,
+    postgres_conn_id = "redshift",
+    sql = SqlQueries.staging_songs_table_create
+)
+
+create_time_table_task = PostgresOperator(
+    task_id = "Create Time Table, if not exists",
+    dag = dag,
+    postgres_conn_id = "redshift",
+    sql = SqlQueries.time_table_create
+)
+
+create_user_table_task = PostgresOperator(
+    task_id = "Create User Table, if not exists",
+    dag = dag,
+    postgres_conn_id = "redshift",
+    sql = SqlQueries.user_table_create
+)
+
+create_artist_table_task = PostgresOperator(
+    task_id = "Create Artist Table, if not exists",
+    dag = dag,
+    postgres_conn_id = "redshift",
+    sql = SqlQueries.artist_table_create
+)
+
+create_song_table_task = PostgresOperator(
+    task_id = "Create Song Table, if not exists",
+    dag = dag,
+    postgres_conn_id = "redshift",
+    sql = SqlQueries.song_table_create
+)
+
+create_songplay_table_task = PostgresOperator(
+    task_id = "Create Songplay Table, if not exists",
+    dag = dag,
+    postgres_conn_id = "redshift",
+    sql = SqlQueries.songplay_table_create
+)
+
+wait_for_table_creation_op = DummyOperator(
+    task_id = "Wait for table creation",
+    dag = dag
+)
+
+#################################################  S3 To Staging Tables Tasks ################################
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
     dag=dag
@@ -32,6 +98,7 @@ stage_songs_to_redshift = StageToRedshiftOperator(
     dag=dag
 )
 
+###########################################  Loading to Fact and Dim Tables Tasks #########################
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
     dag=dag
@@ -57,9 +124,47 @@ load_time_dimension_table = LoadDimensionOperator(
     dag=dag
 )
 
+######################################################## Quality Check ###########################################
+
+
+
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
     dag=dag
 )
 
-end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+end_operator = DummyOperator(
+    task_id='Stop_execution',
+    dag=dag
+)
+
+# Define the dependencies of tasks
+start_operator >> create_staging_events_table_task
+start_operator >> create_staging_songs_table_task
+start_operator >> create_time_table_task
+start_operator >> create_user_table_task
+start_operator >> create_artist_table_task
+start_operator >> create_song_table_task
+start_operator >> create_songplay_table_task
+
+create_staging_events_table_task >> wait_for_table_creation_op
+create_staging_songs_table_task >> wait_for_table_creation_op
+create_time_table_task >> wait_for_table_creation_op
+create_time_table_task >> wait_for_table_creation_op
+create_user_table_task >> wait_for_table_creation_op
+create_artist_table_task >> wait_for_table_creation_op
+
+wait_for_table_creation_op >> stage_events_to_redshift
+wait_for_table_creation_op >> stage_songs_to_redshift
+
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+
+load_songplays_table >> load_user_dimension_table >> run_quality_checks
+load_songplays_table >> load_song_dimension_table >> run_quality_checks
+load_songplays_table >> load_artist_dimension_table >> run_quality_checks
+load_songplays_table >> load_time_dimension_table >> run_quality_checks
+
+run_quality_checks >> end_operator
+
+
--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -1,4 +1,120 @@
+STAGING_TABLE_NAME_EVENT = "staging_events"
+STAGING_TABLE_NAME_SONG = "staging_songs"
+TABLE_NAME_SONGPLAY = "songplays"
+TABLE_NAME_USER = "users"
+TABLE_NAME_SONG = "songs"
+TABLE_NAME_ARTIST = "artists"
+TABLE_NAME_TIME = "time_table"
+
 class SqlQueries:
+    # Table Creation queries
+    staging_events_table_create= f"""
+        CREATE TABLE IF NOT EXISTS {STAGING_TABLE_NAME_EVENT}(
+            artist              VARCHAR(MAX),
+            auth                VARCHAR(MAX),
+            firstName           VARCHAR(MAX),
+            gender              VARCHAR(MAX),
+            itemInSession       INT,
+            lastName            VARCHAR(MAX),
+            length              FLOAT,
+            level               VARCHAR(MAX),
+            location            VARCHAR(MAX),
+            method              VARCHAR(MAX),
+            page                VARCHAR(MAX),
+            registration        TIMESTAMP,
+            sessionId           INT,
+            song                VARCHAR(MAX),
+            status              INT,
+            ts                  TIMESTAMP,
+            userAgent           VARCHAR(MAX),
+            userId              INT
+        );
+        """
+
+    staging_songs_table_create = f"""
+        CREATE TABLE IF NOT EXISTS {STAGING_TABLE_NAME_SONG}(
+            artist_id           VARCHAR(MAX),
+            artist_latitude     FLOAT, 
+            artist_location     VARCHAR(MAX),
+            artist_longitude    FLOAT,
+            artist_name         VARCHAR(MAX),
+            duration            FLOAT,
+            num_songs           INT,
+            song_id             VARCHAR(MAX),
+            title               VARCHAR(MAX),
+            year                INT
+        );
+    """
+
+    songplay_table_create = f"""
+        CREATE TABLE IF NOT EXISTS {TABLE_NAME_SONGPLAY}(
+            songplay_id         INT                 IDENTITY(0,1),
+            start_time          TIMESTAMP,
+            user_id             INT,
+            level               VARCHAR(MAX),
+            song_id             VARCHAR(MAX)        DISTKEY,
+            artist_id           VARCHAR(MAX),
+            session_id          INT,
+            location            VARCHAR(MAX),
+            user_agent          VARCHAR(MAX),
+
+            FOREIGN KEY (start_time) REFERENCES time_table(start_time),
+            FOREIGN KEY (user_id) REFERENCES {TABLE_NAME_USER}(user_id),
+            FOREIGN KEY (song_id) REFERENCES {TABLE_NAME_SONG}(song_id),
+            FOREIGN KEY (artist_id) REFERENCES {TABLE_NAME_ARTIST}(artist_id)
+        );
+    """
+
+
+    user_table_create = f"""
+        CREATE TABLE IF NOT EXISTS {TABLE_NAME_USER}(
+            user_id             INT                 PRIMARY KEY,
+            first_name          VARCHAR(MAX),
+            last_name           VARCHAR(MAX),
+            gender              VARCHAR(MAX),
+            level               VARCHAR(MAX)
+        )
+        DISTSTYLE AUTO;
+        """
+
+    song_table_create = f"""
+        CREATE TABLE IF NOT EXISTS {TABLE_NAME_SONG}(
+            song_id             VARCHAR(MAX)        PRIMARY KEY     DISTKEY,
+            title               VARCHAR(MAX),
+            artist_id           VARCHAR(MAX),
+            year                INT,
+            duration            FLOAT,
+
+            FOREIGN KEY (artist_id) REFERENCES {TABLE_NAME_ARTIST}(artist_id)
+        );
+        """
+
+    artist_table_create = f"""
+        CREATE TABLE IF NOT EXISTS {TABLE_NAME_ARTIST}(
+            artist_id           VARCHAR(MAX)        PRIMARY KEY,
+            name                VARCHAR(MAX),
+            location            VARCHAR(MAX),
+            latitude            VARCHAR(MAX),
+            longitude           VARCHAR(MAX)
+        )
+        DISTSTYLE AUTO;
+        """
+
+    time_table_create = f"""
+        CREATE TABLE IF NOT EXISTS {TABLE_NAME_TIME}(
+            start_time          TIMESTAMP           PRIMARY KEY,
+            hour                INT,
+            day                 INT,
+            week                INT,
+            month               INT,
+            year                INT,
+            weekday             INT
+        )
+        DISTSTYLE AUTO;
+        """
+
+
+    # table insertion queries
     songplay_table_insert = ("""
         SELECT
                 md5(events.sessionid || events.start_time) songplay_id,
