f22ecc8829b23616205520f4f58f10a47cda172e
Hazem Khairy <hazem.hassan.kh@gmail.com>
2023-05-31 03:36:29 +0000

    Made LoadDimension Operator have an option to truncate the table

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -6,22 +6,38 @@ from helpers import SqlQueries
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
+    dim_sql_insert = """
+        INSERT INTO {}
+        {}
+        ;
+    """
+    truncate_table_sql = """
+        TRUNCATE TABLE {};
+    """
     @apply_defaults
     def __init__(self,
                  table= "",
                  redshift_conn_id = "",
                  table_sepcific_sql = "",
+                 truncate_table = True,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.table = table
         self.table_sepcific_sql = table_sepcific_sql
+        self.truncate_table = truncate_table
 
     def execute(self, context):
         postgres_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        sql = LoadDimensionOperator.dim_sql_insert
+        if self.truncate_table:
+            truncate_table_sql = LoadDimensionOperator.truncate_table_sql.format(self.table)
+            sql = truncate_table_sql + '\n' + sql
+        
         postgres_hook.run(
-            SqlQueries.redshift_sql_insert.format(
+            sql.format(
                 self.table,
                 self.table_sepcific_sql
             )
